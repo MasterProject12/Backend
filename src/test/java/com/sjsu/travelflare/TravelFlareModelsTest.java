94575e4f14a240d20e5e3ea9a8d8b29612ac4e2d
shtomar-adb <shtomar@adobe.com>
2021-09-02 02:04:00 -0700

    Updated Unit test.

--- src/test/java/com/sjsu/travelflare/TravelFlareModelsTest.java
+++ src/test/java/com/sjsu/travelflare/TravelFlareModelsTest.java
@@ -4,14 +4,12 @@ import com.sjsu.travelflare.aws.AWSConfigurations;
 import com.sjsu.travelflare.models.geocode.ReverseGeocode;
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.servlet.context.ServletWebServerApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@SpringBootTest
-@RunWith(SpringJUnit4ClassRunner.class)
+//@SpringBootTest
+//@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {
         TravelFlareApplication.class, ServletWebServerApplicationContext.class, AWSConfigurations.class
 })
